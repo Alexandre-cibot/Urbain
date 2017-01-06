82098ce4903dca6db494aa4eb8888935adcd80b0
Alexandre Cibot <alexandre.cibot@gmail.com>
2017-01-06 16:51:10 +0100

    stockage des photos et affichage

--- client/main.js
+++ client/main.js
@@ -1,22 +1,28 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
-
+import {Photos} from "../lib/model/Photos";
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
+// Template.hello.onCreated(function helloOnCreated() {
+//   // counter starts at 0
+//   this.counter = new ReactiveVar(0);
+// });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
+// Template.hello.helpers({
+//   counter() {
+//     return Template.instance().counter.get();
+//   },
+// });
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
+// Template.hello.events({
+//   'click button'(event, instance) {
+//     // increment the counter when button is clicked
+//     instance.counter.set(instance.counter.get() + 1);
+//   },
+// });
+var nb_img = 1; 
+Template.Displayer.helpers({
+	allImages: function () {
+		return Photos.find();
+	},
+})
--- server/main.js
+++ server/main.js
@@ -1,5 +1,15 @@
 import { Meteor } from 'meteor/meteor';
+import {Photos} from "../lib/model/Photos";
+
 
 Meteor.startup(() => {
   // code to run on server at startup
+  // photos = new Mongo.Collection('photos');
+  if(Photos.find().count() == 0) {
+  	  for(var i = 1; i <= 33; i++) {
+  		Photos.insert({"img":"images/photos/" + i + ".jpg"})
+  	}
+  }
+
+
 });
