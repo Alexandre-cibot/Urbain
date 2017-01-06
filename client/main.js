d3e25ddb2cf7acfd63adc39fb2c169ab6858c250
Alexandre Cibot <alexandre.cibot@gmail.com>
2017-01-06 19:48:20 +0100

    Ajout du début des sessions

--- client/main.js
+++ client/main.js
@@ -3,23 +3,6 @@ import { ReactiveVar } from 'meteor/reactive-var';
 import {Photos} from "../lib/model/Photos";
 import './main.html';
 
-// Template.hello.onCreated(function helloOnCreated() {
-//   // counter starts at 0
-//   this.counter = new ReactiveVar(0);
-// });
-
-// Template.hello.helpers({
-//   counter() {
-//     return Template.instance().counter.get();
-//   },
-// });
-
-// Template.hello.events({
-//   'click button'(event, instance) {
-//     // increment the counter when button is clicked
-//     instance.counter.set(instance.counter.get() + 1);
-//   },
-// });
 Template.Displayer.helpers({
 	allImages: function () {
 		return Photos.find();
@@ -29,9 +12,11 @@ Template.Displayer.helpers({
 // TODO Remplacer les logo ---> Vert / blanc
 Template.Navigation.events({
 	'click .home': function(event){
-		alert('Click sur home')
+		//Session.set("currentPage", "Displayer");
+		 alert('Click sur home')
 	},
 	'click .heart': function(event){
+		//Session.set("currentPage", "Displayer");
 		alert('click sur heart')
 	},
 	'click .star': function(event){
